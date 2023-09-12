d7216a37623ea77462ecda28f6bd45bfb5dd290d
Jcaob <jcaobcatalan943@gmail.com>
2023-09-12 12:07:16 -0400

    ShoeMania update 3

--- model/model.js
+++ model/model.js
@@ -22,7 +22,54 @@ var homeContent
         </div>
     </section>
 
-    <section class="questions"></section>
+    <section class="questions">
+        <h1 class="q-head">Frequently Asked Questions</h1>
+        <p class="q-des">We gathered the answers to some popular questions below. If you can't find your question below feel free to contact us,<br> and we'll be happy to help.</p>
+        <div class="question-holder">
+            <div class="question-col" >
+                <div class="question">
+                    <p class="question-name">Do you ship internationally?</p>
+                    <p class="question-answer">Yes, we are pleased to confirm that we offer worldwide shipping! Please note that we are unable to deliver to some countries and we apologize for any inconvenience caused.</p>
+                </div>
+                <div class="question">
+                    <p class="question-name"> Where do you ship from?</p>
+                    <p class="question-answer"> We ship all of our orders from the Netherlands, where our warehouse is based.</p>
+                </div>
+                <div class="question">
+                    <p class="question-name">Do you offer free returns?</p>
+                    <p class="question-answer">We offer a refund on any items returned to us within 14 days of the delivery date, excluding any delivery costs. To request a return, go to Order Management.</p>
+                </div>
+            </div >
+            <div class="question-col">
+                <div class="question">
+                    <p class="question-name">Where do you ship from?</p>
+                    <p class="question-answer">We ship all of our orders from the Netherlands, where our warehouse is based.</p>
+                </div>
+                <div class="question">
+                    <p class="question-name">Do you offer free returns?</p>
+                    <p class="question-answer">We offer a refund on any items returned to us within 14 days of the delivery date, excluding any delivery costs. To request a return, go to Order Management.</p>
+                </div>
+                <div class="question">
+                    <p class="question-name">Do you ship internationally?</p>
+                    <p class="question-answer">Yes, we are pleased to confirm that we offer worldwide shipping! Please note that we are unable to deliver to some countries and we apologize for any inconvenience caused.</p>
+                </div>
+            </div>
+            <div class="question-col">
+                <div class="question">
+                    <p class="question-name">Do you offer free returns?</p>
+                    <p class="question-answer">We offer a refund on any items returned to us within 14 days of the delivery date, excluding any delivery costs. To request a return, go to Order Management.</p>
+                </div>
+                <div class="question">
+                    <p class="question-name">Do you ship internationally?</p>
+                    <p class="question-answer">Yes, we are pleased to confirm that we offer worldwide shipping! Please note that we are unable to deliver to some countries and we apologize for any inconvenience caused.</p>
+                </div>
+                <div class="question">
+                    <p class="question-name">Where do you ship from?</p>
+                    <p class="question-answer">We ship all of our orders from the Netherlands, where our warehouse is based.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
 `;
 
